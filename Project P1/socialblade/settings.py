b9e05a5b4f1283fde51c62301049f829b88b37a0
amirZZZkiller <amirZZZkiller@gmail.com>
2024-06-09 15:23:34 +0330

    rename repo files' names

---
+++ Project P1/socialblade/settings.py
@@ -0,0 +1,75 @@
+# Scrapy settings for socialblade project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = "socialblade"
+
+SPIDER_MODULES = ["socialblade.spiders"]
+NEWSPIDER_MODULE = "socialblade.spiders"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 16
+
+# Configure a delay for requests for the same website (default: 0)
+DOWNLOAD_DELAY = 1.5
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Language': 'en',
+}
+
+# Enable or disable spider middlewares
+SPIDER_MIDDLEWARES = {
+    'socialblade.middlewares.SocialbladeSpiderMiddleware': 543,
+}
+
+# Enable or disable downloader middlewares
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
+
+# Configure item pipelines
+ITEM_PIPELINES = {
+    'socialblade.pipelines.SocialbladePipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+
+# Enable and configure HTTP caching (disabled by default)
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
+
+FEEDS = {
+    'output.csv': {
+        'format': 'csv',
+        'encoding': 'utf8',
+        'store_empty': False,
+        'fields': None,
+        'indent': 4,
+    },
+}
